74699e6cf9777fd08f122fdf3cdd28d62ff85de5
Yichen Lu <litash529@gmail.com>
2017-02-01 11:13:13 +0000

    changed file structure, ready to split development into components

--- js/controller.js
+++ js/controller.js
@@ -1,3 +1,9 @@
+// Project: VSTS Discovery extension
+// JavaScript functions for controlling main page behaviours
+// Author: Yichen Lu
+// Create Date: 31-Jan-2017
+// Update Date: 01-Feb-2017
+
 $('#status').click(function(event) {
     event.preventDefault();
     $('#100-points-components').load("components/100-points-status.html");
