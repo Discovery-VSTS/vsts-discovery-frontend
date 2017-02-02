9244650f6e131a453054d6a3977d423d01d052c9
Yichen Lu <litash529@gmail.com>
2017-02-02 00:00:55 +0000

    added menu item behavior controls; added new pie chart

--- js/100-points-status-ctrl.js
+++ js/100-points-status-ctrl.js
@@ -3,41 +3,78 @@
 // Author: Yichen Lu
 // Create Date: 01-Feb-2017
 // Update Date: 01-Feb-2017
-
-var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+function load100PtStatus() {
+    $('#100-points-components').load("components/100-points-status.html", function(response, status, xhr){
+        if (status == "success") {
+            var ctx = $("#myChart");
+            var ctx2 = $("#myChart2");
+            var myChart = new Chart(ctx2, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                            }
+                        }]
+                    }
                 }
-            }]
+            });
+
+
+            var data = {
+                labels: [
+                    "Red",
+                    "Blue",
+                    "Yellow"
+                ],
+                datasets: [
+                    {
+                        data: [300, 50, 100],
+                        backgroundColor: [
+                            "#FF6384",
+                            "#36A2EB",
+                            "#FFCE56"
+                        ],
+                        hoverBackgroundColor: [
+                            "#FF6384",
+                            "#36A2EB",
+                            "#FFCE56"
+                        ]
+                    }]
+            };
+
+            var myPieChart = new Chart(ctx,{
+                type: 'pie',
+                data: data,
+                options: {
+                    responsive: false
+                }
+            });
         }
-    }
-});
+    });
+}
--- js/controller.js
+++ js/controller.js
@@ -4,12 +4,24 @@
 // Create Date: 31-Jan-2017
 // Update Date: 01-Feb-2017
 
+jQuery(document).ready(function() {
+    $($('#navbar ul').children().get(0)).addClass('active');
+});
+
 $('#status').click(function(event) {
     event.preventDefault();
-    $('#100-points-components').load("components/100-points-status.html");
+    removeActiveClassInMenuItems();
+    $(this).addClass('active');
+    load100PtStatus();
 });
 
 $('#assign').click(function(event) {
     event.preventDefault();
+    removeActiveClassInMenuItems();
+    $(this).addClass('active');
     $('#100-points-components').load("components/100-points-assign.html");
 });
+
+function removeActiveClassInMenuItems() {
+    $("#100-points-menu li").removeClass('active');
+}
