c20d6b6b821e464328a41eb9ffc378d68c4ed77d
Jason Martyres <Jason Martyres>
2017-02-28 23:22:18 +0000

    code cleaned, removed unneccesary files, available points added

--- js/100-points-assign-ctrl.js
+++ js/100-points-assign-ctrl.js
@@ -1,8 +1,8 @@
-serverURL = "http://127.0.0.1:8000"         //testing URL
-//serverURL = "https://138.68.147.100:8000"   //live URL
+//serverURL = "http://127.0.0.1:8000"         //testing URL
+serverURL = "https://138.68.147.100:8000"   //live URL
 
-var currentUser = "jason@jason.com";   //testing currentUser
-//var currentUser = vssWebContext.user.email/ /live
+//var currentUser = "krinal@krinal.com";        //testing currentUser
+var currentUser = vssWebContext.user.email //live
 
 function getMembers() {
     var xhr = new XMLHttpRequest();
@@ -12,9 +12,8 @@ function getMembers() {
     return JSON.parse(xhr.responseText);
 }
 
+//generate user list
 var firstdayofweek = firstDayOfWeek();
-
-console.log(firstdayofweek);
 
 var obj = getMembers();
 
@@ -22,56 +21,23 @@ var user_list = $("#user_list");
 
 obj.forEach(function (user, index) {
 
-//     user_list.append("<div id='user_list_div'>" +
-//         "<h1>" + user.name + "</h1>" +
-//         "<input type='number' min='0' max='100' id='user_" + index + "' email = " + user.email + " />" +
-//         "</div>");
-// });
-
-user_list.append("<tr id='user_list_div'>" +
+    user_list.append("<tr id='user_list_div'>" +
         "<td>" + user.name + "</td>" +
-        "<td> <input type='number' min='0' max='100' id='user_" + index + "' email = " + user.email + " />" +
+        "<td> <input class='points-field' type='number' min='0' max='100' id='user_" + index + "' email = " + user.email + " />" +
         "<td>" + "</tr>");
 });
-
-//console.log(user_list)
-
-$("#user_list_div input").on("click", function () {
-    console.log($(this).attr("email"));
-})
-
-// $("#user_list_div input").each(function (index, val) {
-//     // $.each(function(index, val){
-//     //     console.log(index + ": " + $(this).text());    
-//     // })
-//     console.log(index + ": " + $(val).val());
-// });
+//ended user list generation
 
 $('#submitButton').click(function (event) {
-    // $("#user_list_div input").each(function (index, val) {
-    //     $.each(function(index, val){
-    //         console.log(index + ": " + $(this).text());    
-    //     })
-    //     console.log(index + ": " + $(val).val()+$(val).attr("email"));
-    // });
-    //old code before status check
-    //var jsonString = createJSON();
-    //sendPoints(jsonString);
-
-    //start of test
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState !== 4) return;
         if (this.status === 404) { // 404 means the distro doesn't exist so we POST it
-         var jsonString = createJSON();
-     sendPoints("POST", jsonString);
-     alert("points distro created");
+            var jsonString = createJSON();
+            sendPoints("POST", jsonString);
+            alert("points distro created");
         } else if (this.status == 200) { // 200 means the distro exists so PUT updates
-            // previous working code
-            // var jsonString = createJSONUPDATE();
-            // sendPoints("PUT", jsonString);
-            // alert("points updated");
-            if (hasSent()) {
+        	if (hasSent()) {
                 var jsonString = createJSONUPDATE();
                 sendPoints("PUT", jsonString);
                 alert("points updated");
@@ -81,19 +47,18 @@ $('#submitButton').click(function (event) {
                 alert("points distro created");
             }
         } else {
-        	alert("something has gone terribly wrong: error " + this.status); // something else
+            alert("something has gone terribly wrong: error " + this.status); // something else
         }
     };
     xhr.open('GET', serverURL + '/v1/points/distribution/2017-02-27/', true);
     xhr.send();
-    //end of test
 });
 
 $('#validateButton').click(function (event) {
 	validatePoints();
+	alert("not all members gave points to colleagues");
 });
 
-//WILL CLEAR UP
 function createJSON() {
     jsonObj = [];
     $("#user_list_div input").each(function () {
@@ -113,11 +78,9 @@ function createJSON() {
 
         jsonObj.push(item);
     });
-    //console.log(jsonString = JSON.stringify(jsonObj));
     return jsonObj;
 }
 
-//MUST CLEAN UP AS createJSON()
 function createJSONUPDATE() {
     jsonObj = [];
     $("#user_list_div input").each(function () {
@@ -126,7 +89,7 @@ function createJSONUPDATE() {
         var points = $(this).val();
 
         if (isNaN(parseInt(points))) {
-        	return;
+            return;
         }
 
         item = {};
@@ -141,30 +104,8 @@ function createJSONUPDATE() {
 
         jsonObj.push(item);
     });
-    //console.log(jsonString = JSON.stringify(jsonObj));
     return jsonObj;
 }
-
-//// old code
-// function createJSON() {
-// 	jsonObj = [];
-// 	$("#user_list_div input").each(function() {
-
-// 		var id = $(this).attr("email");
-// 		var points = $(this).val();
-
-// 		item = {};
-// 		item ["to_member"] = id;
-// 		item ["points"] = parseInt(points);
-// 		item ["from_member"] = "jason@jason.com";
-// 		item ["week"] = "2017-02-27";
-
-// 		jsonObj.push(item);
-// 	});
-// 	//console.log(jsonString = JSON.stringify(jsonObj));
-// 	return jsonObj;
-// }
-
 
 //req POST for initial distribution or PUT to update
 function sendPoints(REQ, jsonString) {
@@ -175,22 +116,8 @@ function sendPoints(REQ, jsonString) {
         "given_points": jsonString,
         "week": firstdayofweek
     }
-    //console.log(JSON.stringify(json));
     xhr.send(JSON.stringify(json));
 }
-
-// function updatePoints(jsonString) {
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("PUT", "http://127.0.0.1:8000/v1/points/distribution/send/");
-//     xhr.setRequestHeader("Content-Type", "application/json");
-//     var json = {
-//         "given_points": jsonString,
-//         "week": firstdayofweek
-//     }
-//     //console.log(JSON.stringify(json));
-//     xhr.send(JSON.stringify(json));
-//     console.log(xhr.statusText);
-// }
 
 function validatePoints() {
     var xhr = new XMLHttpRequest();
@@ -199,8 +126,7 @@ function validatePoints() {
     var json = {
         "week": firstdayofweek
     }
-     console.log(JSON.stringify(json));
-    //xhr.send(JSON.stringify(json));
+    xhr.send(JSON.stringify(json));
 }
 
 function getPointsForWeek() {
@@ -211,18 +137,6 @@ function getPointsForWeek() {
     console.log(xhr.status);
     return JSON.parse(xhr.responseText);
 }
-
-// function hasSent() {
-// 	var array = getPointsForWeek();
-//     for (var i = 0; i < array.given_points.length; i++) {
-//         console.log(array.given_points[i].from_member);
-//         if (array.given_points[i].from_member == currentUser) {
-//             return true;
-//             break;
-//         }
-//         return false;
-//     }
-// }
 
 function hasSent() {
     var array = getPointsForWeek();
@@ -235,44 +149,36 @@ function hasSent() {
     return false;
 }
 
-	////duplicate??
-	// function sendPoints(jsonString) {
-	//     var xhr = new XMLHttpRequest();
-	//     xhr.open("POST", "http://127.0.0.1:8000/v1/points/distribution/send/");
-	//     xhr.setRequestHeader("Content-Type", "application/json");
-	//     var json = {
-	//         "given_points": jsonString,
-	//         "week": "2017-02-27"
-	//     }
-	//     //console.log(JSON.stringify(json));
-	//     xhr.send(JSON.stringify(json));
-	// }
+//Return the first day of the current week (Sunday)
+function firstDayOfWeek() {
+    var curr = new Date;
+    var firstday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 1));
+    return firstday.toISOString().substring(0, 10);
+}
 
-	// function sendPoints() {
-	//     var xhr = new XMLHttpRequest();
-	//     xhr.open("POST", "http://127.0.0.1:8000/v1/points/distribution/send/");
-	//     xhr.setRequestHeader("Content-Type", "application/json");
-	//     xhr.send(JSON.stringify({
-	//         "given_points": [
-	//             {
-	//                 "to_member": "krinal@krinal.com",
-	//                 "points": 100,
-	//                 "from_member": "jason@jason.com",
-	//                 "week": "2017-02-20"
-	//             },
-	//             {
-	//                 "to_member": "jason@jason.com",
-	//                 "points": 100,
-	//                 "from_member": "jason@jason.com",
-	//                 "week": "2017-02-20"
-	//             },
-	//             {
-	//                 "to_member": "hello@hello.com",
-	//                 "points": 100,
-	//                 "from_member": "jason@jason.com",
-	//                 "week": "2017-02-20"
-	//             }
-	//         ],
-	//         "week": "2017-02-20"
-	//     }));
-	// }
+//Return the last day of the current week (Saturday)
+function lastDayOfWeek() {
+    var curr = new Date;
+    var lastday = new Date(curr.setDate(curr.getDate() - curr.getDay() + 7));
+    return lastday.toISOString().substring(0, 10);
+}
+
+//Return date formatted
+function displayFirstAndLastDate() {
+    return firstDayOfWeek() + " to " + lastDayOfWeek();
+}
+
+$('input.points-field').change(function(event) {
+    var usedPoints = 0;
+    $('.points-field').each(function(index, el) {
+        var fieldPoint;
+        if ($(el).val()=="") {
+            fieldPoint = 0;
+        }else{
+            fieldPoint = parseInt($(el).val());
+        }
+        usedPoints += fieldPoint;
+        console.log(usedPoints)
+    });
+    $('#pointValue').text(100-usedPoints);
+});
